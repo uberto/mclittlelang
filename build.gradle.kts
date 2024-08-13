1e52b14cff4f084fd576e31538f1e1ace922072f
uberto <uberto.gama@gmail.com>
2024-08-13 11:57:39 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("multiplatform") version "2.0.10"
+}
+
+group = "com.ubertob.mclittlelang"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
+                runTask?.args("myFirstArg mySecondArg")
+            }
+        }
+    }
+
+    sourceSets {
+        nativeMain.dependencies {
+//            implementation(libs.kotlinxSerializationJson)
+        }
+    }
+}
---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,13 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(args: Array<String>) {
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+
+    args.onEach {
+        println(it)
+    }
+}
